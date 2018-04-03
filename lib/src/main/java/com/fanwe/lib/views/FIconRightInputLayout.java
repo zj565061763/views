d0fa3e1ead2eeb142745805fad84f5b50ef51b90
zjun <565061763@qq.com>
2018-04-03 09:27:24 +0800

    重构

--- lib/src/main/java/com/fanwe/lib/views/FIconRightInputLayout.java
+++ lib/src/main/java/com/fanwe/lib/views/FIconRightInputLayout.java
@@ -1,10 +1,8 @@
 package com.fanwe.lib.views;
 
 import android.content.Context;
-import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.Gravity;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -13,21 +11,21 @@ import android.widget.ImageView;
 /**
  * Created by zhengjun on 2018/4/2.
  */
-public abstract class FIconRightEditText extends FrameLayout
+public class FIconRightInputLayout extends FrameLayout
 {
-    public FIconRightEditText(Context context)
+    public FIconRightInputLayout(Context context)
     {
         super(context);
         init();
     }
 
-    public FIconRightEditText(Context context, AttributeSet attrs)
+    public FIconRightInputLayout(Context context, AttributeSet attrs)
     {
         super(context, attrs);
         init();
     }
 
-    public FIconRightEditText(Context context, AttributeSet attrs, int defStyleAttr)
+    public FIconRightInputLayout(Context context, AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
         init();
@@ -38,27 +36,51 @@ public abstract class FIconRightEditText extends FrameLayout
 
     private void init()
     {
-        addView(getEditText());
-        addView(getImageViewRight());
     }
 
     public EditText getEditText()
     {
-        if (mEditText == null)
-        {
-            final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT);
-
-            mEditText = new EditText(getContext());
-            mEditText.setLayoutParams(params);
-        }
         return mEditText;
+    }
+
+    public void setEditText(EditText editText)
+    {
+        if (mEditText != editText)
+        {
+            if (mEditText != null)
+            {
+                removeView(mEditText);
+            }
+
+            mEditText = editText;
+
+            if (editText != null)
+            {
+                addEditText(editText);
+            }
+        }
+    }
+
+    private void addEditText(EditText editText)
+    {
+        ViewGroup.LayoutParams params = editText.getLayoutParams();
+        if (params == null)
+        {
+            editText.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+        }
+
+        removeView(getImageViewRight());
+        addView(editText);
+        addView(getImageViewRight());
     }
 
     public ImageView getImageViewRight()
     {
         if (mImageViewRight == null)
         {
+            mImageViewRight = new ImageView(getContext());
+
             final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
             params.gravity = Gravity.RIGHT | Gravity.CENTER_VERTICAL;
@@ -66,7 +88,6 @@ public abstract class FIconRightEditText extends FrameLayout
             final float scale = getResources().getDisplayMetrics().density;
             params.rightMargin = (int) (scale * 10);
 
-            mImageViewRight = new ImageView(getContext());
             mImageViewRight.setLayoutParams(params);
         }
         return mImageViewRight;
