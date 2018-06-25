21aada144c0426861a4f0311683ee7aebb85b874
zjun <565061763@qq.com>
2018-06-25 11:46:41 +0800

    FFullRecyclerView

---
+++ lib/src/main/java/com/fanwe/lib/views/FFullRecyclerView.java
@@ -0,0 +1,45 @@
+package com.fanwe.lib.views;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * 一次性全部展示的RecyclerView，需要在android.support.v4.widget.NestedScrollView里面使用
+ */
+public class FFullRecyclerView extends FRecyclerView
+{
+    public FFullRecyclerView(Context context)
+    {
+        super(context);
+        init();
+    }
+
+    public FFullRecyclerView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init();
+    }
+
+    public FFullRecyclerView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private boolean mIsFocusable;
+
+    private void init()
+    {
+        setNestedScrollingEnabled(false);
+    }
+
+    @Override
+    public void setAdapter(RecyclerView.Adapter adapter)
+    {
+        mIsFocusable = isFocusable();
+        setFocusable(false);
+        super.setAdapter(adapter);
+        setFocusable(mIsFocusable);
+    }
+}
