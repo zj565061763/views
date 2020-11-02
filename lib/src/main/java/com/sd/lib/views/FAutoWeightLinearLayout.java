502129d7b42729617b631e1d8f690bef32488d39
zjun <565061763@qq.com>
2020-11-02 12:00:31 +0800

    FAutoWeightLinearLayout

---
+++ lib/src/main/java/com/sd/lib/views/FAutoWeightLinearLayout.java
@@ -0,0 +1,120 @@
+package com.sd.lib.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class FAutoWeightLinearLayout extends LinearLayout
+{
+    public FAutoWeightLinearLayout(Context context)
+    {
+        super(context);
+    }
+
+    public FAutoWeightLinearLayout(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public FAutoWeightLinearLayout(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private View mHeaderView;
+    private View mFooterView;
+
+    private SizeHandler mSizeHandler;
+
+    private SizeHandler getSizeHandler()
+    {
+        if (mSizeHandler == null)
+            mSizeHandler = getOrientation() == HORIZONTAL ? new HorizontalSizeHandler() : new VerticalSizeHandler();
+        return mSizeHandler;
+    }
+
+    @Override
+    public void setOrientation(int orientation)
+    {
+        if (getOrientation() != orientation)
+            mSizeHandler = null;
+
+        super.setOrientation(orientation);
+    }
+
+    @Override
+    protected void onFinishInflate()
+    {
+        super.onFinishInflate();
+        if (getChildCount() != 2)
+            throw new RuntimeException("child count must be 2");
+
+        mHeaderView = getChildAt(0);
+        mFooterView = getChildAt(1);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        setWeight(0, mHeaderView);
+        setWeight(0, mFooterView);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        getSizeHandler().onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    private abstract class SizeHandler
+    {
+        private void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+        {
+            final int totalSize = getSize(FAutoWeightLinearLayout.this);
+            final int headerSize = getSize(mHeaderView);
+            final int footerSize = getSize(mFooterView);
+            final int leftSize = totalSize - footerSize;
+            if (headerSize >= leftSize)
+            {
+                if (setWeight(1, mHeaderView))
+                    FAutoWeightLinearLayout.super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            } else
+            {
+                if (setWeight(0, mHeaderView))
+                    FAutoWeightLinearLayout.super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            }
+        }
+
+        protected abstract int getSize(View view);
+    }
+
+    private final class HorizontalSizeHandler extends SizeHandler
+    {
+        @Override
+        protected int getSize(View view)
+        {
+            return view.getMeasuredWidth();
+        }
+    }
+
+    private final class VerticalSizeHandler extends SizeHandler
+    {
+        @Override
+        protected int getSize(View view)
+        {
+            return view.getMeasuredHeight();
+        }
+    }
+
+    private static boolean setWeight(int weight, View view)
+    {
+        final LinearLayout.LayoutParams params = (LayoutParams) view.getLayoutParams();
+        if (params == null)
+            return false;
+
+        if (params.weight != weight)
+        {
+            params.weight = weight;
+            return true;
+        }
+        return false;
+    }
+}
