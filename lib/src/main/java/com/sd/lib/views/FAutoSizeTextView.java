64293146dcd7604757ffab7ffcf244ee9869bfdc
zjun <565061763@qq.com>
2020-12-16 22:27:02 +0800

    FAutoSizeTextView

--- lib/src/main/java/com/sd/lib/views/FAutoSizeTextView.java
+++ lib/src/main/java/com/sd/lib/views/FAutoSizeTextView.java
@@ -10,21 +10,27 @@ import androidx.core.widget.TextViewCompat;
 
 public class FAutoSizeTextView extends AppCompatTextView
 {
+    public FAutoSizeTextView(Context context)
+    {
+        super(context);
+        init(context, null);
+    }
+
     public FAutoSizeTextView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
-        initAttributeSet(context, attrs);
+        init(context, attrs);
     }
 
     public FAutoSizeTextView(Context context, AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
-        initAttributeSet(context, attrs);
+        init(context, attrs);
     }
 
-    private void initAttributeSet(Context context, AttributeSet attrs)
+    private void init(Context context, AttributeSet attrs)
     {
-        if (context == null || attrs == null)
+        if (context == null)
             return;
 
         boolean applyAutoSize = true;
@@ -32,32 +38,37 @@ public class FAutoSizeTextView extends AppCompatTextView
         float autoSizeMaxTextSizeInPx = dp2px(500, context);
         float autoSizeStepGranularityInPx = dp2px(1, context);
 
-        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AppCompatTextView);
-        try
+
+        if (attrs != null)
         {
-            if (a.hasValue(R.styleable.AppCompatTextView_autoSizeTextType))
+            final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AppCompatTextView);
+            try
             {
-                final int type = a.getInt(R.styleable.AppCompatTextView_autoSizeTextType, TextViewCompat.AUTO_SIZE_TEXT_TYPE_NONE);
-                if (type == TextViewCompat.AUTO_SIZE_TEXT_TYPE_NONE)
-                    applyAutoSize = false;
-            }
+                if (a.hasValue(R.styleable.AppCompatTextView_autoSizeTextType))
+                {
+                    final int type = a.getInt(R.styleable.AppCompatTextView_autoSizeTextType, TextViewCompat.AUTO_SIZE_TEXT_TYPE_NONE);
+                    if (type == TextViewCompat.AUTO_SIZE_TEXT_TYPE_NONE)
+                        applyAutoSize = false;
+                }
 
-            if (applyAutoSize)
+                if (applyAutoSize)
+                {
+                    if (a.hasValue(R.styleable.AppCompatTextView_autoSizeMinTextSize))
+                        autoSizeMinTextSizeInPx = a.getDimension(R.styleable.AppCompatTextView_autoSizeMinTextSize, autoSizeMinTextSizeInPx);
+
+                    if (a.hasValue(R.styleable.AppCompatTextView_autoSizeMaxTextSize))
+                        autoSizeMaxTextSizeInPx = a.getDimension(R.styleable.AppCompatTextView_autoSizeMaxTextSize, autoSizeMaxTextSizeInPx);
+
+                    if (a.hasValue(R.styleable.AppCompatTextView_autoSizeStepGranularity))
+                        autoSizeStepGranularityInPx = a.getDimension(R.styleable.AppCompatTextView_autoSizeStepGranularity, autoSizeStepGranularityInPx);
+                }
+            } finally
             {
-                if (a.hasValue(R.styleable.AppCompatTextView_autoSizeMinTextSize))
-                    autoSizeMinTextSizeInPx = a.getDimension(R.styleable.AppCompatTextView_autoSizeMinTextSize, autoSizeMinTextSizeInPx);
-
-                if (a.hasValue(R.styleable.AppCompatTextView_autoSizeMaxTextSize))
-                    autoSizeMaxTextSizeInPx = a.getDimension(R.styleable.AppCompatTextView_autoSizeMaxTextSize, autoSizeMaxTextSizeInPx);
-
-                if (a.hasValue(R.styleable.AppCompatTextView_autoSizeStepGranularity))
-                    autoSizeStepGranularityInPx = a.getDimension(R.styleable.AppCompatTextView_autoSizeStepGranularity, autoSizeStepGranularityInPx);
+                if (a != null)
+                    a.recycle();
             }
-        } finally
-        {
-            if (a != null)
-                a.recycle();
         }
+
 
         if (applyAutoSize)
         {
