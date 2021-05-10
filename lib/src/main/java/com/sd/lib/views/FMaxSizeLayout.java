08d126288ea940b616aeb903abc2ecc23689b296
zjun <565061763@qq.com>
2021-05-10 23:38:31 +0800

    FMaxSizeLayout

--- lib/src/main/java/com/sd/lib/views/FMaxSizeLayout.java
+++ lib/src/main/java/com/sd/lib/views/FMaxSizeLayout.java
@@ -11,15 +11,19 @@ import androidx.annotation.Nullable;
  * 限制最大宽高的布局
  */
 public class FMaxSizeLayout extends FrameLayout {
+    /** 最大宽度 */
     private Integer mMaxWidth = null;
+    /** 最大高度 */
     private Integer mMaxHeight = null;
+    /** 是否检查{@link MeasureSpec#EXACTLY} */
+    private boolean mCheckMeasureExactly = false;
 
     public FMaxSizeLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
     /**
-     * 设置最大高度
+     * 设置最大宽度
      *
      * @param maxWidth
      */
@@ -50,6 +54,18 @@ public class FMaxSizeLayout extends FrameLayout {
         }
     }
 
+    /**
+     * 设置是否检查{@link MeasureSpec#EXACTLY}，默认不检查
+     *
+     * @param checkMeasureExactly
+     */
+    public void setCheckMeasureExactly(boolean checkMeasureExactly) {
+        if (mCheckMeasureExactly != checkMeasureExactly) {
+            mCheckMeasureExactly = checkMeasureExactly;
+            requestLayout();
+        }
+    }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         if (mMaxWidth != null || mMaxHeight != null) {
@@ -60,7 +76,7 @@ public class FMaxSizeLayout extends FrameLayout {
         }
     }
 
-    private static int getSpec(int measureSpec, Integer maxSize) {
+    private int getSpec(int measureSpec, Integer maxSize) {
         if (maxSize == null) {
             return measureSpec;
         }
@@ -69,7 +85,9 @@ public class FMaxSizeLayout extends FrameLayout {
 
         final int size = MeasureSpec.getSize(measureSpec);
         final int mode = MeasureSpec.getMode(measureSpec);
-        if (mode == MeasureSpec.EXACTLY || mode == MeasureSpec.AT_MOST) {
+
+        final boolean checkExactly = mCheckMeasureExactly && mode == MeasureSpec.EXACTLY;
+        if (mode == MeasureSpec.AT_MOST || checkExactly) {
             if (size > maxSize) {
                 resultSpec = MeasureSpec.makeMeasureSpec(maxSize, mode);
             }
