7ac4ae70b4f30efa2122a7f055e8928e8022d6ce
zjun <565061763@qq.com>
2021-05-10 22:22:35 +0800

    FMaxSizeLayout

--- lib/src/main/java/com/sd/lib/views/FMaxSizeLayout.java
+++ lib/src/main/java/com/sd/lib/views/FMaxSizeLayout.java
@@ -8,10 +8,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class FMaxSizeLayout extends FrameLayout {
+
+    private Integer mMaxWidth = null;
     private Integer mMaxHeight = null;
 
     public FMaxSizeLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    /**
+     * 设置最大高度
+     *
+     * @param maxWidth
+     */
+    public void setMaxWidth(Integer maxWidth) {
+        if (maxWidth != null && maxWidth < 0) {
+            maxWidth = 0;
+        }
+
+        if (mMaxWidth != maxWidth) {
+            mMaxWidth = maxWidth;
+            requestLayout();
+        }
     }
 
     /**
@@ -34,12 +52,23 @@ public class FMaxSizeLayout extends FrameLayout {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        if (mMaxHeight != null) {
-            final int height = getMeasuredHeight();
-            if (height > mMaxHeight) {
-                final int heightSpec = MeasureSpec.makeMeasureSpec(mMaxHeight, MeasureSpec.AT_MOST);
-                super.onMeasure(widthMeasureSpec, heightSpec);
+        if (mMaxWidth != null || mMaxHeight != null) {
+            int widthSpec = widthMeasureSpec;
+            int heightSpec = heightMeasureSpec;
+
+            if (mMaxWidth != null) {
+                if (getMeasuredWidth() > mMaxWidth) {
+                    widthSpec = MeasureSpec.makeMeasureSpec(mMaxWidth, MeasureSpec.AT_MOST);
+                }
             }
+
+            if (mMaxHeight != null) {
+                if (getMeasuredHeight() > mMaxHeight) {
+                    heightSpec = MeasureSpec.makeMeasureSpec(mMaxHeight, MeasureSpec.AT_MOST);
+                }
+            }
+
+            super.onMeasure(widthSpec, heightSpec);
         }
     }
 }
