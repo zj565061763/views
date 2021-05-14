f6526b895680e889fc698991ec01106ac5cc1cb9
zjun <565061763@qq.com>
2021-05-14 13:26:07 +0800

    rename

--- app/src/main/java/com/sd/views/TextLineLayoutActivity.kt
+++ app/src/main/java/com/sd/views/TextLineLayoutActivity.kt
@@ -2,16 +2,16 @@ package com.sd.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.sd.views.databinding.ActivityTextHeightLayoutBinding
+import com.sd.views.databinding.ActivityTextLineLayoutBinding
 
 class TextLineLayoutActivity : AppCompatActivity() {
     val TAG = TextLineLayoutActivity::class.java.simpleName
 
-    private lateinit var _binding: ActivityTextHeightLayoutBinding
+    private lateinit var _binding: ActivityTextLineLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityTextHeightLayoutBinding.inflate(layoutInflater)
+        _binding = ActivityTextLineLayoutBinding.inflate(layoutInflater)
         setContentView(_binding.root)
     }
 }
