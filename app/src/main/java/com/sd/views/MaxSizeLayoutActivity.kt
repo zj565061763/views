8250db0e75d9f4c553802862c5fbb06028b1f081
zjun <565061763@qq.com>
2021-06-09 11:41:25 +0800

    demo

--- app/src/main/java/com/sd/views/MaxSizeLayoutActivity.kt
+++ app/src/main/java/com/sd/views/MaxSizeLayoutActivity.kt
@@ -7,6 +7,7 @@ import com.sd.views.databinding.ActivityMaxSizeLayoutBinding
 
 class MaxSizeLayoutActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var _binding: ActivityMaxSizeLayoutBinding
+    private val _content = "123456789"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +22,10 @@ class MaxSizeLayoutActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v) {
-            _binding.btnSmall -> _binding.tvContent.text = "123456789"
+            _binding.btnSmall -> _binding.tvContent.text = _content
             _binding.btnBig -> {
                 val text = _binding.tvContent.text.toString()
-                _binding.tvContent.text = text + text
+                _binding.tvContent.text = text + _content
             }
         }
     }
