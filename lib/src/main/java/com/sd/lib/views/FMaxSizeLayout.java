42c1aacd35a6c02946334a4ebbf3effe1c2ced51
zjun <565061763@qq.com>
2021-06-09 11:36:58 +0800

    FMaxSizeLayout

--- lib/src/main/java/com/sd/lib/views/FMaxSizeLayout.java
+++ lib/src/main/java/com/sd/lib/views/FMaxSizeLayout.java
@@ -11,27 +11,27 @@ import androidx.annotation.Nullable;
  * 限制最大宽高的布局
  */
 public class FMaxSizeLayout extends FrameLayout {
-    /** 最大宽度 */
-    private Integer mMaxWidth = null;
-    /** 最大高度 */
-    private Integer mMaxHeight = null;
-    /** 是否检查{@link MeasureSpec#EXACTLY} */
-    private boolean mCheckMeasureExactly = false;
+    public FMaxSizeLayout(@NonNull Context context) {
+        super(context);
+    }
 
     public FMaxSizeLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    /**
-     * 设置最大宽度
-     *
-     * @param maxWidth
-     */
-    public void setMaxWidth(Integer maxWidth) {
-        if (maxWidth != null && maxWidth <= 0) {
-            maxWidth = null;
-        }
+    public FMaxSizeLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
+    /** 最大宽度 */
+    private int mMaxWidth = 0;
+    /** 最大高度 */
+    private int mMaxHeight = 0;
+
+    /**
+     * 设置最大宽度，大于0才有效
+     */
+    public void setMaxWidth(int maxWidth) {
         if (mMaxWidth != maxWidth) {
             mMaxWidth = maxWidth;
             requestLayout();
@@ -39,61 +39,40 @@ public class FMaxSizeLayout extends FrameLayout {
     }
 
     /**
-     * 设置最大高度
-     *
-     * @param maxHeight
+     * 设置最大高度，大于0才有效
      */
-    public void setMaxHeight(Integer maxHeight) {
-        if (maxHeight != null && maxHeight <= 0) {
-            maxHeight = null;
-        }
-
+    public void setMaxHeight(int maxHeight) {
         if (mMaxHeight != maxHeight) {
             mMaxHeight = maxHeight;
             requestLayout();
         }
     }
 
-    /**
-     * 设置是否检查{@link MeasureSpec#EXACTLY}，默认不检查
-     *
-     * @param checkMeasureExactly
-     */
-    public void setCheckMeasureExactly(boolean checkMeasureExactly) {
-        if (mCheckMeasureExactly != checkMeasureExactly) {
-            mCheckMeasureExactly = checkMeasureExactly;
-            requestLayout();
-        }
-    }
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        if (mMaxWidth != null || mMaxHeight != null) {
-            super.onMeasure(getSpec(widthMeasureSpec, mMaxWidth),
-                    getSpec(heightMeasureSpec, mMaxHeight));
-        } else {
-            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        }
+        super.onMeasure(getSpec(widthMeasureSpec, mMaxWidth),
+                getSpec(heightMeasureSpec, mMaxHeight));
     }
 
-    private int getSpec(int measureSpec, Integer maxSize) {
-        if (maxSize == null) {
+    private static int getSpec(int measureSpec, int maxSize) {
+        if (maxSize <= 0) {
             return measureSpec;
         }
 
-        int resultSpec = measureSpec;
-
-        final int size = MeasureSpec.getSize(measureSpec);
+        boolean makeMeasureSpec = false;
         final int mode = MeasureSpec.getMode(measureSpec);
-
-        final boolean checkExactly = mCheckMeasureExactly && mode == MeasureSpec.EXACTLY;
-        if (mode == MeasureSpec.AT_MOST || checkExactly) {
-            if (size > maxSize) {
-                resultSpec = MeasureSpec.makeMeasureSpec(maxSize, mode);
-            }
-        } else if (mode == MeasureSpec.UNSPECIFIED) {
-            resultSpec = MeasureSpec.makeMeasureSpec(maxSize, MeasureSpec.AT_MOST);
+        switch (mode) {
+            case MeasureSpec.AT_MOST:
+                final int size = MeasureSpec.getSize(measureSpec);
+                makeMeasureSpec = size > maxSize;
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                makeMeasureSpec = true;
+                break;
         }
-        return resultSpec;
+
+        return makeMeasureSpec ?
+                MeasureSpec.makeMeasureSpec(maxSize, MeasureSpec.AT_MOST) :
+                measureSpec;
     }
 }
