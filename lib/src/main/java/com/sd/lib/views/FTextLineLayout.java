7e4e6ef45692d2a62c235293935ef39b68a1a0e1
zjun <565061763@qq.com>
2021-06-09 14:32:02 +0800

    FTextLineLayout

--- lib/src/main/java/com/sd/lib/views/FTextLineLayout.java
+++ lib/src/main/java/com/sd/lib/views/FTextLineLayout.java
@@ -31,6 +31,9 @@ public class FTextLineLayout extends FrameLayout {
         final TextView textView = mTextView;
         if (textView == null) return;
 
+        final int textViewHeight = textView.getMeasuredHeight();
+        if (textViewHeight <= 0) return;
+
         final int maxLines = textView.getMaxLines();
         if (maxLines <= 0) return;
 
@@ -40,20 +43,18 @@ public class FTextLineLayout extends FrameLayout {
         final int deltaLine = maxLines - totalLine;
         if (deltaLine <= 0) return;
 
-        final int textViewHeight = textView.getMeasuredHeight();
         final int totalPadding = Math.abs(textView.getLayout().getTopPadding()) + Math.abs(textView.getLayout().getBottomPadding());
         final float lineSpacing = textView.getLineSpacingExtra() * textView.getLineSpacingMultiplier();
+        final float totalLineSpacing = (totalLine - 1) * lineSpacing;
 
-        final float totalLineHeight = textViewHeight - totalPadding - ((totalLine - 1) * lineSpacing);
+        final float totalLineHeight = textViewHeight - totalPadding - totalLineSpacing;
         final float lineHeight = totalLineHeight / totalLine;
 
         final float deltaHeight = deltaLine * (lineHeight + lineSpacing);
         final int targetHeight = (int) (textViewHeight + deltaHeight + 0.5);
 
-        if (getMeasuredHeight() < targetHeight) {
-            final int heightSpec = MeasureSpec.makeMeasureSpec(targetHeight, MeasureSpec.EXACTLY);
-            super.onMeasure(widthMeasureSpec, heightSpec);
-        }
+        final int heightSpec = MeasureSpec.makeMeasureSpec(targetHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightSpec);
     }
 
     @Override
